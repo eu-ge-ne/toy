ca263642919eec33b74470934ff94e432b5b15c4
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-17 22:21:39 -0400

    1

---
+++ lib/vt/init.ts
@@ -0,0 +1,23 @@
+import { DECResetMode, decrst, decset, DECSetMode } from "@eu-ge-ne/ctlseqs";
+import { set_flags } from "@eu-ge-ne/kitty-keys";
+
+import { show_cursor } from "./cursor.ts";
+import { flush } from "./write.ts";
+
+export function init_vt(): void {
+  Deno.stdin.setRaw(true);
+
+  flush(
+    decset(DECSetMode.ALTSCR),
+    set_flags({
+      disambiguate: true,
+      alternates: true,
+      all_keys: true,
+      text: true,
+    }),
+  );
+}
+
+export function restore_vt(): void {
+  flush(decrst(DECResetMode.ALTSCR), show_cursor);
+}
