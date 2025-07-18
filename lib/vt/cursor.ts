b41930ee817593937210ddb28ec4a7b2a6be7528
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-17 22:21:06 -0400

    1

---
+++ lib/vt/cursor.ts
@@ -0,0 +1,43 @@
+import {
+  cpr_req,
+  cup,
+  decrc,
+  DECResetMode,
+  decrst,
+  decsc,
+  decset,
+  DECSetMode,
+  parse_cpr_res,
+} from "@eu-ge-ne/ctlseqs";
+
+import { flush } from "./write.ts";
+
+export const save_cursor = decsc;
+export const restore_cursor = decrc;
+
+export const hide_cursor = decrst(DECResetMode.DECTCEM);
+export const show_cursor = decset(DECSetMode.DECTCEM);
+
+export function set_cursor(y: number, x: number): Uint8Array {
+  return cup(y + 1, x + 1);
+}
+
+const buf = new Uint8Array(1024);
+
+export function get_cursor(): [number, number] {
+  flush(cpr_req);
+
+  for (let i = 0; i < 4; i += 1) {
+    const len = Deno.stdin.readSync(buf)!;
+
+    if (len) {
+      const pos = parse_cpr_res(buf.subarray(0, len));
+
+      if (pos) {
+        return [pos[0] - 1, pos[1] - 1];
+      }
+    }
+  }
+
+  throw new Error("get_cursor(): timeout");
+}
