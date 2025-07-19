bbaa28d48f55de9650d3cf0ae640e1c44defea5f
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-18 22:53:45 -0400

    @lib/buf

--- lib/buf/buf.ts
+++ lib/buf/buf.ts
@@ -1,5 +1,5 @@
 import { TextBuf } from "@eu-ge-ne/text-buf";
-import { Grapheme, GraphemeSegmenter } from "@lib/grapheme";
+import { GraphemeSegmenter } from "@lib/grapheme";
 
 export type Snapshot = InstanceType<typeof TextBuf>["root"];
 
@@ -31,30 +31,26 @@ export class Buf {
     return this.#buf.line_count;
   }
 
-  line_text(ln: number): string {
+  line(ln: number): string {
     return this.#buf.read([ln, 0], [ln + 1, 0]);
   }
 
-  line_graphemes(ln: number): IteratorObject<Grapheme> {
-    return this.segmenter.graphemes(this.line_text(ln));
-  }
-
   insert([ln, col]: Pos, text: string): void {
-    const col0 = this.segmenter.unit_index(this.line_text(ln), col);
+    const col0 = this.segmenter.unit_index(this.line(ln), col);
 
     this.#buf.insert([ln, col0], text);
   }
 
   delete([from_ln, from_col]: Pos, [to_ln, to_col]: Pos): void {
-    const col0 = this.segmenter.unit_index(this.line_text(from_ln), from_col);
-    const col1 = this.segmenter.unit_index(this.line_text(to_ln), to_col + 1);
+    const col0 = this.segmenter.unit_index(this.line(from_ln), from_col);
+    const col1 = this.segmenter.unit_index(this.line(to_ln), to_col + 1);
 
     this.#buf.delete([from_ln, col0], [to_ln, col1]);
   }
 
   copy([from_ln, from_col]: Pos, [to_ln, to_col]: Pos): string {
-    const col0 = this.segmenter.unit_index(this.line_text(from_ln), from_col);
-    const col1 = this.segmenter.unit_index(this.line_text(to_ln), to_col + 1);
+    const col0 = this.segmenter.unit_index(this.line(from_ln), from_col);
+    const col1 = this.segmenter.unit_index(this.line(to_ln), to_col + 1);
 
     return this.#buf.read([from_ln, col0], [to_ln, col1]);
   }
