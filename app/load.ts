744ffc474dd04f310c0225ffe95d2ac9e68eadbb
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-21 17:59:21 -0400

    fix: load

--- app/load.ts
+++ app/load.ts
@@ -6,15 +6,26 @@ export class LoadAction extends Action<[string]> {
     const { editor, alert } = this.app;
 
     try {
-      await editor.buffer.load(path);
+      using file = await Deno.open(path, { read: true });
+
+      const info = await file.stat();
+      if (!info.isFile) {
+        throw new Error(`${path} is not a file`);
+      }
+
+      await editor.buffer.load(file);
 
       editor.reset(true);
 
       this.app.set_file_path(path);
     } catch (err) {
-      await alert.open(err);
+      if (err instanceof Deno.errors.NotFound) {
+        this.app.set_file_path(path);
+      } else {
+        await alert.open(err);
 
-      exit();
+        exit();
+      }
     }
   }
 }
