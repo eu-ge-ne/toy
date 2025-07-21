dcc0cf44cc7a4637214121551920d3ef6d457818
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-21 17:59:05 -0400

    fix: save_as

--- app/save-as.ts
+++ app/save-as.ts
@@ -5,17 +5,23 @@ export class SaveAsAction extends Action<[]> {
     const { editor, file_path, alert, save_as } = this.app;
 
     while (true) {
-      const path = await save_as.open(file_path);
-      if (path.length === 0) {
+      const new_path = await save_as.open(file_path);
+      if (!new_path) {
         return;
       }
 
       try {
-        await editor.buffer.save(file_path);
+        using file = await Deno.open(new_path, {
+          create: true,
+          write: true,
+          truncate: true,
+        });
+
+        await editor.buffer.save(file);
 
         editor.reset(false);
 
-        this.app.set_file_path(path);
+        this.app.set_file_path(new_path);
       } catch (err) {
         await alert.open(err);
 
