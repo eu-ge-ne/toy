5794798aaf2dd10b3d3f2b1958edaa020db66a68
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-24 17:29:48 -0400

    fix: @lib/input

--- lib/input/input.ts
+++ lib/input/input.ts
@@ -2,16 +2,8 @@ export type { Key } from "@eu-ge-ne/kitty-keys";
 
 import { Key, parse_keys } from "@eu-ge-ne/kitty-keys";
 
-// TODO: improve
 export async function* read_input(): AsyncGenerator<Key | string | Uint8Array> {
-  const buf = new Uint8Array(1024);
-
-  while (true) {
-    const len = await Deno.stdin.read(buf);
-    if (len === null) {
-      break;
-    }
-
-    yield* parse_keys(buf.subarray(0, len));
+  for await (const bytes of Deno.stdin.readable) {
+    yield* parse_keys(bytes);
   }
 }
