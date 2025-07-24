27a993248d18da15e1f41032410a2c53ffa88f1e
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-24 16:42:26 -0400

    fix

--- lib/input/input.ts
+++ lib/input/input.ts
@@ -2,6 +2,7 @@ export type { Key } from "@eu-ge-ne/kitty-keys";
 
 import { Key, parse_keys } from "@eu-ge-ne/kitty-keys";
 
+// TODO: improve
 export async function* read_input(): AsyncGenerator<Key | string | Uint8Array> {
   const buf = new Uint8Array(1024);
 
