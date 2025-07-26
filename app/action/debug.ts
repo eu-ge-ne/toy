372122c275f6abc279f193e57a413fd97fdedbc7
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-25 22:12:21 -0400

    fix

--- app/action/debug.ts
+++ app/action/debug.ts
@@ -1,4 +1,4 @@
-import { Action } from "../action.ts";
+import { Action } from "./action.ts";
 
 export class DebugAction extends Action {
   keys = [
@@ -6,6 +6,11 @@ export class DebugAction extends Action {
   ];
 
   protected override async _run(): Promise<void> {
+    const { actions_started } = this.app;
+    if (actions_started > 1) {
+      return;
+    }
+
     const { debug, editor } = this.app.ui;
 
     debug.enabled = !debug.enabled;
