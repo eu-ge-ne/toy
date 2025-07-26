cdbb62c0c7828685d3d90f664674e6b02c365d49
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-26 16:07:22 -0400

    fix

--- app/commands/select-all.ts
+++ app/commands/select-all.ts
@@ -1,12 +1,14 @@
 import { Command } from "./command.ts";
 
 export class SelectAllCommand extends Command {
+  override name = "SelectAll";
+
   keys = [
     { name: "a", ctrl: true },
     { name: "a", super: true },
   ];
 
-  protected override async command(): Promise<void> {
+  async command(): Promise<void> {
     const editor = this.app.active_editor;
     if (!editor?.enabled) {
       return;
--- app/commands/tab.ts
+++ app/commands/tab.ts
@@ -1,11 +1,13 @@
 import { Command } from "./command.ts";
 
 export class TabCommand extends Command {
+  override name = "Tab";
+
   keys = [
     { name: "TAB" },
   ];
 
-  protected override async command(): Promise<void> {
+  async command(): Promise<void> {
     const editor = this.app.active_editor;
     if (!editor?.enabled) {
       return;
