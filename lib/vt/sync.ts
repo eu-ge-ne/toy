0c77ab0042d4a0d309219e3e7514e94c58c8cbbb
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-28 15:51:08 -0400

    fix: bsu, esu

--- lib/vt/sync.ts
+++ lib/vt/sync.ts
@@ -1,39 +1,24 @@
 import { DECResetMode, decrst, decset, DECSetMode } from "@eu-ge-ne/ctlseqs";
 
-import { direct_write } from "./write.ts";
+import { write_direct } from "./write.ts";
 
-export const bsu = decset(DECSetMode.BSU);
-export const esu = decrst(DECResetMode.ESU);
+const bsu_bytes = decset(DECSetMode.BSU);
+const esu_bytes = decrst(DECResetMode.ESU);
 
-const buf = new ArrayBuffer(1024 * 64, { maxByteLength: 1024 * 1024 * 64 });
-const bytes = new Uint8Array(buf);
-
-let i = 0;
 let c = 0;
 
-export function begin_sync_write(...chunks: Uint8Array[]): void {
-  c += 1;
-  sync_write(...chunks);
-}
-
-export function sync_write(...chunks: Uint8Array[]): void {
-  for (const chunk of chunks) {
-    const j = i + chunk.byteLength;
-    if (j > buf.byteLength) {
-      buf.resize(j * 2);
-    }
-
-    bytes.set(chunk, i);
-    i = j;
+export function bsu(): void {
+  if (c === 0) {
+    write_direct(bsu_bytes);
   }
+
+  c += 1;
 }
 
-export function end_sync_write(...chunks: Uint8Array[]): void {
-  sync_write(...chunks);
+export function esu(): void {
   c -= 1;
 
   if (c === 0) {
-    direct_write(bsu, bytes.subarray(0, i), esu);
-    i = 0;
+    write_direct(esu_bytes);
   }
 }
