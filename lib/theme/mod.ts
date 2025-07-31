08f04e3eeca6ac97b749c39947bf7077213dd0b3
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-31 07:04:41 -0400

    1

--- lib/theme/mod.ts
+++ lib/theme/mod.ts
@@ -1,5 +1,6 @@
 export * as alert from "./alert.ts";
 export * as ask from "./ask.ts";
+export * from "./colors.ts";
 export * as debug from "./debug.ts";
 export * as editor from "./editor.ts";
 export * as footer from "./footer.ts";
--- lib/theme/switch.ts
+++ lib/theme/switch.ts
@@ -1,16 +1,16 @@
 import * as alert from "./alert.ts";
 import * as ask from "./alert.ts";
+import { Colors } from "./colors.ts";
 import * as debug from "./debug.ts";
 import * as editor from "./editor.ts";
 import * as footer from "./footer.ts";
 import * as header from "./header.ts";
 import * as palette from "./palette.ts";
 import * as save_as from "./save-as.ts";
-
-import { Colors, set_colors } from "./tokens.ts";
+import { set_tokens } from "./tokens.ts";
 
 export function switch_theme(colors: Colors): void {
-  set_colors(colors);
+  set_tokens(colors);
 
   alert.init();
   ask.init();
