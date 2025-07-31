22141e6babfdb006e67e479e19c0f4ea6a37c626
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-31 03:50:32 -0400

    1

--- lib/theme/mod.ts
+++ lib/theme/mod.ts
@@ -2,7 +2,7 @@ export * as alert from "./alert.ts";
 export * as ask from "./ask.ts";
 export * as debug from "./debug.ts";
 export * as editor from "./editor.ts";
-export * from "./footer.ts";
+export * as footer from "./footer.ts";
 export * from "./header.ts";
 export * from "./save-as.ts";
 export * from "./palette.ts";
