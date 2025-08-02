b689a6567e4326020998dc0823b5ad9d0bcfb549
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-08-02 19:41:21 -0400

    fix: ui/debug

--- ui/debug/debug.ts
+++ ui/debug/debug.ts
@@ -1,32 +1,26 @@
+import { clamp } from "@lib/std";
 import { debug as theme } from "@lib/theme";
 import { Area, Control } from "@lib/ui";
 import * as vt from "@lib/vt";
 
-const WIDTH = 30;
-const HEIGHT = 7;
 const MIB = Math.pow(1024, 2);
 
 export class Debug extends Control {
   #input_time = "0";
   #render_time = "0";
 
-  override resize(area: Area): void {
-    super.resize(
-      new Area(
-        area.x0 + area.w - WIDTH,
-        area.y0 + area.h - HEIGHT,
-        WIDTH,
-        HEIGHT,
-      ),
-    );
+  layout({ y, x, w, h }: Area): void {
+    this.w = clamp(30, 0, w);
+    this.h = clamp(7, 0, h);
+
+    this.y = y + h - this.h;
+    this.x = x + w - this.w;
   }
 
   render(): void {
     if (!this.enabled) {
       return;
     }
-
-    const { y0, x0, h, w } = this.area;
 
     const mem = Deno.memoryUsage();
     const rss = (mem.rss / MIB).toFixed();
@@ -39,40 +33,40 @@ export class Debug extends Control {
     vt.flush_buf(
       vt.cursor.save,
       theme.BACKGROUND,
-      ...vt.clear(y0, x0, h, w),
+      ...vt.clear(this),
       theme.TEXT,
-      vt.cursor.set(y0 + 1, x0 + 1),
+      vt.cursor.set(this.y + 1, this.x + 1),
       ...vt.fmt.text(
-        { len: w - 1 },
+        { len: this.w - 1 },
         "Input    : ",
         this.#input_time,
         " ms",
       ),
-      vt.cursor.set(y0 + 2, x0 + 1),
+      vt.cursor.set(this.y + 2, this.x + 1),
       ...vt.fmt.text(
-        { len: w - 1 },
+        { len: this.w - 1 },
         "Render   : ",
         this.#render_time,
         " ms",
       ),
-      vt.cursor.set(y0 + 3, x0 + 1),
+      vt.cursor.set(this.y + 3, this.x + 1),
       ...vt.fmt.text(
-        { len: w - 1 },
+        { len: this.w - 1 },
         "RSS      : ",
         rss,
       ),
-      vt.cursor.set(y0 + 4, x0 + 1),
+      vt.cursor.set(this.y + 4, this.x + 1),
       ...vt.fmt.text(
-        { len: w - 1 },
+        { len: this.w - 1 },
         "Heap     : ",
         heap_used,
         "/",
         heap_total,
         " MiB",
       ),
-      vt.cursor.set(y0 + 5, x0 + 1),
+      vt.cursor.set(this.y + 5, this.x + 1),
       ...vt.fmt.text(
-        { len: w - 1 },
+        { len: this.w - 1 },
         "External : ",
         external_mem,
         " MiB",
