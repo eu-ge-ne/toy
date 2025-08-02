767a6f23594273652475f3065e0fcf54b07139fd
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-08-02 18:51:17 -0400

    refactor: Control.proto.layout

--- lib/vt/clear.ts
+++ lib/vt/clear.ts
@@ -2,12 +2,14 @@ import { cud1, ech } from "@eu-ge-ne/ctlseqs";
 
 import * as cursor from "./cursor.ts";
 
-export function* clear(
-  y: number,
-  x: number,
-  h: number,
-  w: number,
-): Generator<Uint8Array> {
+interface ClearParams {
+  y: number;
+  x: number;
+  w: number;
+  h: number;
+}
+
+export function* clear({ y, x, w, h }: ClearParams): Generator<Uint8Array> {
   yield cursor.set(y, x);
 
   for (let i = h; i > 0; i -= 1) {
