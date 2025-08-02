6385daebd8c59a0e3c1c13ee7ef43df1b804070c
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-08-02 18:53:54 -0400

    fix: @lib/ui

--- lib/ui/area.ts
+++ lib/ui/area.ts
@@ -1,53 +1,6 @@
-export class Area {
-  readonly x1: number;
-  readonly y1: number;
-
-  constructor(
-    readonly x0: number,
-    readonly y0: number,
-    readonly w: number,
-    readonly h: number,
-  ) {
-    this.w = w;
-    this.h = h;
-    this.x1 = x0 + w;
-    this.y1 = y0 + h;
-  }
-
-  static from_screen(): Area {
-    const { columns, rows } = Deno.consoleSize();
-    return new Area(0, 0, columns, rows);
-  }
-
-  div_x(w: number): [Area, Area] {
-    if (w < 0) {
-      w = this.w + w;
-    }
-
-    return [
-      new Area(this.x0, this.y0, w, this.h),
-      new Area(this.x0 + w, this.y0, this.w - w, this.h),
-    ];
-  }
-
-  div_y(h: number): [Area, Area] {
-    if (h < 0) {
-      h = this.h + h;
-    }
-
-    return [
-      new Area(this.x0, this.y0, this.w, h),
-      new Area(this.x0, this.y0 + h, this.w, this.h - h),
-    ];
-  }
-
-  center(area: Area): Area {
-    const w = Math.min(area.w, this.w);
-    const h = Math.min(area.h, this.h);
-
-    const x0 = Math.trunc((this.w - w) / 2);
-    const y0 = Math.trunc((this.h - h) / 2);
-
-    return new Area(x0, y0, w, h);
-  }
+export interface Area {
+  y: number;
+  x: number;
+  w: number;
+  h: number;
 }
