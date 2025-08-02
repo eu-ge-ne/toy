a4d0e497b5d2d87453a31fa2eecae9f7e3ccd55b
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-08-02 19:39:01 -0400

    fix: lib/ui

--- lib/ui/modal.ts
+++ lib/ui/modal.ts
@@ -1,7 +1,5 @@
 import { Control } from "./control.ts";
 
 export abstract class Modal<P extends unknown[], R> extends Control {
-  protected done!: PromiseWithResolvers<R>;
-
   abstract open(...params: P): Promise<R>;
 }
