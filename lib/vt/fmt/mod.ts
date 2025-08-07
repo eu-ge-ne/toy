c902f5ebe3bb441d4ca8eccfa446df27eede06ce
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-08-06 23:10:24 -0400

    1

--- lib/vt/fmt/mod.ts
+++ lib/vt/fmt/mod.ts
@@ -1,4 +1,3 @@
 export * from "./center.ts";
 export * from "./fit.ts";
-export * from "./space.ts";
 export * from "./span.ts";
--- lib/vt/fmt/space.ts
+++ lib/vt/fmt/space.ts
@@ -1,14 +1,6 @@
-import { Span } from "./span.ts";
-
 let bytes = new Uint8Array(1024).fill(0x20);
 
-export function space(span: Span, len: number): Uint8Array {
-  if (len > span.len) {
-    throw new Error("len > span.len");
-  }
-
-  span.len -= len;
-
+export function space(len: number): Uint8Array {
   if (len < bytes.length) {
     return bytes.subarray(0, len);
   }
