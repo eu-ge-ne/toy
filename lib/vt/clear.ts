5f172455557bdbd44351a941d21a2273422ca953
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-08-09 01:13:31 -0400

    1

--- lib/vt/clear.ts
+++ lib/vt/clear.ts
@@ -1,12 +1,14 @@
-import { cud1, ech } from "@eu-ge-ne/ctlseqs";
-
+import { csi, csi_cached } from "./ansi.ts";
 import * as cursor from "./cursor.ts";
+
+const ech = (n: number) => csi_cached(n, () => `${n}X`);
+const cud = csi("B");
 
 export function clear_line(w: number): Uint8Array {
   return ech(w);
 }
 
-interface ClearParams {
+interface ClearAreaParams {
   y: number;
   x: number;
   w: number;
@@ -14,13 +16,12 @@ interface ClearParams {
 }
 
 export function* clear_area(
-  { y, x, w, h }: ClearParams,
+  { y, x, w, h }: ClearAreaParams,
 ): Generator<Uint8Array> {
   yield cursor.set(y, x);
 
   for (let i = h; i > 0; i -= 1) {
     yield ech(w);
-
-    yield cud1;
+    yield cud;
   }
 }
