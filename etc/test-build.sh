97d58e37c79a04868d5b0e8162641eea3a20428b
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-03 18:36:22 -0500

    fix

--- etc/test-build.sh
+++ etc/test-build.sh
@@ -9,11 +9,11 @@ deno compile --quiet --frozen --reload --output tmp/toy -ERW main.ts
 ACTUAL="$(./tmp/toy -v)"
 
 if [[ $ACTUAL = "$EXPECTED" ]]; then
-    echo Test build - ok:
-    echo Output: "$ACTUAL"
+    echo Test build - ok
+    echo "    Output: $ACTUAL"
 else
-    echo Test build - fail:
-    echo Expected: "$EXPECTED"
-    echo Actual  : "$ACTUAL"
+    echo Test build - fail
+    echo "    Expected: $EXPECTED"
+    echo "    Actual  : $ACTUAL"
     exit 1
 fi
