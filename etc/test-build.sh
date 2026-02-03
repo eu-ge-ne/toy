77eab8b20f2ef2d96e680b7e647b2e0339cccffc
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-02 22:18:55 -0500

    chore: test-build.sh (#127)

---
+++ etc/test-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+APP_V=$(deno ./etc/version.ts)
+DENO_V=$(deno -v)
+EXPECTED="toy $APP_V ($DENO_V)"
+
+deno compile --frozen --reload --output tmp/toy -ERW main.ts
+
+ACTUAL="$(./tmp/toy -v)"
+
+if [[ $ACTUAL = $EXPECTED ]]; then
+    echo Test build - ok
+else
+    echo Test build - fail:
+    echo Expected: $EXPECTED
+    echo Actual  : $ACTUAL
+    exit 1
+fi
