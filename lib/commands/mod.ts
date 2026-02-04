ae36027596432200290d56646bd6fe00637ddf7e
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-03 23:08:49 -0500

    1

--- lib/commands/mod.ts
+++ lib/commands/mod.ts
@@ -1,19 +1,42 @@
-export * from "./copy.ts";
-export * from "./cut.ts";
-export * from "./debug.ts";
-export * from "./exit.ts";
-export * from "./palette.ts";
-export * from "./paste.ts";
-export * from "./redo.ts";
-export * from "./save.ts";
-export * from "./select-all.ts";
-export * from "./theme-base16.ts";
-export * from "./theme-gray.ts";
-export * from "./theme-neutral.ts";
-export * from "./theme-slate.ts";
-export * from "./theme-stone.ts";
-export * from "./theme-zinc.ts";
-export * from "./undo.ts";
-export * from "./whitespace.ts";
-export * from "./wrap.ts";
-export * from "./zen.ts";
+import { Command } from "./command.ts";
+import { CopyCommand } from "./copy.ts";
+import { CutCommand } from "./cut.ts";
+import { DebugCommand } from "./debug.ts";
+import { ExitCommand } from "./exit.ts";
+import { PaletteCommand } from "./palette.ts";
+import { PasteCommand } from "./paste.ts";
+import { RedoCommand } from "./redo.ts";
+import { SaveCommand } from "./save.ts";
+import { SelectAllCommand } from "./select-all.ts";
+import { ThemeBase16Command } from "./theme-base16.ts";
+import { ThemeGrayCommand } from "./theme-gray.ts";
+import { ThemeNeutralCommand } from "./theme-neutral.ts";
+import { ThemeSlateCommand } from "./theme-slate.ts";
+import { ThemeStoneCommand } from "./theme-stone.ts";
+import { ThemeZincCommand } from "./theme-zinc.ts";
+import { UndoCommand } from "./undo.ts";
+import { WhitespaceCommand } from "./whitespace.ts";
+import { WrapCommand } from "./wrap.ts";
+import { ZenCommand } from "./zen.ts";
+
+export const commands: Command[] = [
+  CopyCommand,
+  CutCommand,
+  DebugCommand,
+  ExitCommand,
+  PaletteCommand,
+  PasteCommand,
+  RedoCommand,
+  SaveCommand,
+  SelectAllCommand,
+  ThemeBase16Command,
+  ThemeGrayCommand,
+  ThemeNeutralCommand,
+  ThemeSlateCommand,
+  ThemeStoneCommand,
+  ThemeZincCommand,
+  UndoCommand,
+  WhitespaceCommand,
+  WrapCommand,
+  ZenCommand,
+];
