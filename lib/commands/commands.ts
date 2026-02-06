b484f5e1a9a4032d88eb2152e17a8468ece20e0c
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-05 21:46:52 -0500

    1

--- lib/commands/commands.ts
+++ lib/commands/commands.ts
@@ -1,21 +1,99 @@
-export interface Command {}
+export interface Command<T = undefined> {
+  name: string;
+  data: T;
+}
 
-export const Copy: Command = {};
-export const Cut: Command = {};
-export const Debug: Command = {};
-export const Exit: Command = {};
-export const Palette: Command = {};
-export const Paste: Command = {};
-export const Redo: Command = {};
-export const Save: Command = {};
-export const SelectAll: Command = {};
-export const ThemeBase16: Command = {};
-export const ThemeGray: Command = {};
-export const ThemeNeutral: Command = {};
-export const ThemeSlate: Command = {};
-export const ThemeStone: Command = {};
-export const ThemeZinc: Command = {};
-export const Undo: Command = {};
-export const Whitespace: Command = {};
-export const Wrap: Command = {};
-export const Zen: Command = {};
+export const Copy: Command = {
+  name: "Copy",
+  data: undefined,
+};
+
+export const Cut: Command = {
+  name: "Cut",
+  data: undefined,
+};
+
+export const Debug: Command = {
+  name: "Debug",
+  data: undefined,
+};
+
+export const Exit: Command = {
+  name: "Exit",
+  data: undefined,
+};
+
+export const Palette: Command = {
+  name: "Palette",
+  data: undefined,
+};
+
+export const Paste: Command = {
+  name: "Paste",
+  data: undefined,
+};
+
+export const Redo: Command = {
+  name: "Redo",
+  data: undefined,
+};
+
+export const Save: Command = {
+  name: "Save",
+  data: undefined,
+};
+
+export const SelectAll: Command = {
+  name: "SelectAll",
+  data: undefined,
+};
+
+export const ThemeBase16: Command = {
+  name: "ThemeBase16",
+  data: undefined,
+};
+
+export const ThemeGray: Command = {
+  name: "ThemeGray",
+  data: undefined,
+};
+
+export const ThemeNeutral: Command = {
+  name: "ThemeNeutral",
+  data: undefined,
+};
+
+export const ThemeSlate: Command = {
+  name: "ThemeSlate",
+  data: undefined,
+};
+
+export const ThemeStone: Command = {
+  name: "ThemeStone",
+  data: undefined,
+};
+
+export const ThemeZinc: Command = {
+  name: "ThemeZinc",
+  data: undefined,
+};
+
+export const Undo: Command = {
+  name: "Undo",
+  data: undefined,
+};
+
+export const Whitespace: Command = {
+  name: "Whitespace",
+  data: undefined,
+};
+
+export const Wrap: Command = {
+  name: "Wrap",
+  data: undefined,
+};
+
+export const Zen: Command = {
+  name: "Zen",
+  data: undefined,
+};
