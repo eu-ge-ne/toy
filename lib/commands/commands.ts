84ced97e19691f6a2c47c04161b6fcf7ab716b6d
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-05 22:54:26 -0500

    1

--- lib/commands/commands.ts
+++ lib/commands/commands.ts
@@ -1,99 +1,32 @@
-export interface Command<T = undefined> {
-  name: string;
-  data: T;
-}
+import { Themes } from "@lib/themes";
 
-export const Copy: Command = {
-  name: "Copy",
-  data: undefined,
-};
-
-export const Cut: Command = {
-  name: "Cut",
-  data: undefined,
-};
-
-export const Debug: Command = {
-  name: "Debug",
-  data: undefined,
-};
-
-export const Exit: Command = {
-  name: "Exit",
-  data: undefined,
-};
-
-export const Palette: Command = {
-  name: "Palette",
-  data: undefined,
-};
-
-export const Paste: Command = {
-  name: "Paste",
-  data: undefined,
-};
-
-export const Redo: Command = {
-  name: "Redo",
-  data: undefined,
-};
-
-export const Save: Command = {
-  name: "Save",
-  data: undefined,
-};
-
-export const SelectAll: Command = {
-  name: "SelectAll",
-  data: undefined,
-};
-
-export const ThemeBase16: Command = {
-  name: "ThemeBase16",
-  data: undefined,
-};
-
-export const ThemeGray: Command = {
-  name: "ThemeGray",
-  data: undefined,
-};
-
-export const ThemeNeutral: Command = {
-  name: "ThemeNeutral",
-  data: undefined,
-};
-
-export const ThemeSlate: Command = {
-  name: "ThemeSlate",
-  data: undefined,
-};
-
-export const ThemeStone: Command = {
-  name: "ThemeStone",
-  data: undefined,
-};
-
-export const ThemeZinc: Command = {
-  name: "ThemeZinc",
-  data: undefined,
-};
-
-export const Undo: Command = {
-  name: "Undo",
-  data: undefined,
-};
-
-export const Whitespace: Command = {
-  name: "Whitespace",
-  data: undefined,
-};
-
-export const Wrap: Command = {
-  name: "Wrap",
-  data: undefined,
-};
-
-export const Zen: Command = {
-  name: "Zen",
-  data: undefined,
+export type Command = {
+  name: "Copy";
+} | {
+  name: "Cut";
+} | {
+  name: "Debug";
+} | {
+  name: "Exit";
+} | {
+  name: "Palette";
+} | {
+  name: "Paste";
+} | {
+  name: "Redo";
+} | {
+  name: "Save";
+} | {
+  name: "SelectAll";
+} | {
+  name: "Theme";
+  data: keyof typeof Themes;
+} | {
+  name: "Undo";
+} | {
+  name: "Whitespace";
+} | {
+  name: "Wrap";
+} | {
+  name: "Zen";
 };
