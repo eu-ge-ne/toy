5e4c4701dad8a1b8f7a43180eaa1fe9fce08d84e
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-08 21:34:27 -0500

    1

--- lib/vt/wchar.ts
+++ lib/vt/wchar.ts
@@ -1,36 +1,21 @@
 import { CSI } from "./ansi.ts";
 import { set } from "./cursor.ts";
+import { readRegExp } from "./read.ts";
 import { sync } from "./writer.ts";
 
 export const cpr_req = CSI("6n");
-
-const dec = new TextDecoder();
-const buf = new Uint8Array(1024);
 
 export function wchar(y: number, x: number, bytes: Uint8Array): number {
   set(sync, y, x);
   sync.write(bytes);
   sync.write(cpr_req);
 
-  const t0 = Date.now();
+  const match = readRegExp(/\x1b\[\d+;(\d+)R/);
 
-  while (true) {
-    const len = Deno.stdin.readSync(buf);
-    if (!len) {
-      throw new Error("wchar error");
-    }
-
-    const match = dec.decode(buf.subarray(0, len)).match(/\x1b\[\d+;(\d+)R/);
-    if (match) {
-      const x1 = Number.parseInt(match[1]!);
-      if (!Number.isSafeInteger(x1)) {
-        throw new Error("wchar error");
-      }
-      return x1 - 1 - x;
-    }
-
-    if (Date.now() - t0 > 10) {
-      throw new Error("wchar error");
-    }
+  const x1 = Number.parseInt(match[1]!);
+  if (!Number.isSafeInteger(x1)) {
+    throw new Error("wchar error");
   }
+
+  return x1 - 1 - x;
 }
