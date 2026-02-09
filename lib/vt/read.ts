1b3eb63f8e19bd39b1e02fd0f2dda23af7498e04
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-08 21:58:41 -0500

    1

--- lib/vt/read.ts
+++ lib/vt/read.ts
@@ -5,50 +5,54 @@ let i = 0;
 
 export async function readKey(): Promise<Key> {
   while (true) {
-    const n = await Deno.stdin.read(buf.subarray(i));
-    if (typeof n === "number") {
-      i += n;
-    } else {
+    const data = await readAsync();
+
+    const parsed = Key.parse(data);
+    if (!parsed) {
       continue;
     }
 
-    const data = buf.subarray(0, i);
+    const chunk = data.subarray(parsed[1]);
+    buf.set(chunk);
+    i = chunk.byteLength;
 
-    const parsed = Key.parse(data);
-    if (parsed) {
-      const chunk = data.subarray(parsed[1]);
-
-      buf.set(chunk);
-      i = chunk.byteLength;
-
-      return parsed[0];
-    }
+    return parsed[0];
   }
 }
 
 const dec = new TextDecoder();
 
 export function readRegExp(re: RegExp): RegExpMatchArray {
-  for (let attemt = 0; attemt < 10; attemt += 1) {
-    const n = Deno.stdin.readSync(buf.subarray(i));
-    if (typeof n === "number") {
-      i += n;
-    } else {
+  for (let i = 0; i < 10; i += 1) {
+    const data = readSync();
+
+    const match = dec.decode(data).match(re);
+    if (!match) {
       continue;
     }
 
-    const data = buf.subarray(0, i);
+    const chunk = data.subarray(match.index! + match[0].length);
+    buf.set(chunk);
+    i = chunk.byteLength;
 
-    const match = dec.decode(data).match(re);
-    if (match) {
-      const chunk = data.subarray(match.index! + match[0].length);
-
-      buf.set(chunk);
-      i = chunk.byteLength;
-
-      return match;
-    }
+    return match;
   }
 
   throw new Error(`readRegExp error: [${buf.subarray(0, i)}]`);
+}
+
+async function readAsync(): Promise<Uint8Array> {
+  const n = await Deno.stdin.read(buf.subarray(i));
+  if (typeof n === "number") {
+    i += n;
+  }
+  return buf.subarray(0, i);
+}
+
+function readSync(): Uint8Array {
+  const n = Deno.stdin.readSync(buf.subarray(i));
+  if (typeof n === "number") {
+    i += n;
+  }
+  return buf.subarray(0, i);
 }
