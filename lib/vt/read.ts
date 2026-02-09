77b057c4c4be648327f22d92f3544ba29f0fdd7e
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-08 22:48:15 -0500

    1

--- lib/vt/read.ts
+++ lib/vt/read.ts
@@ -5,54 +5,44 @@ let i = 0;
 
 export async function readKey(): Promise<Key> {
   while (true) {
-    const data = await readAsync();
-
+    const data = buf.subarray(0, i);
     const parsed = Key.parse(data);
-    if (!parsed) {
-      continue;
+
+    if (parsed) {
+      const chunk = data.subarray(parsed[1]);
+      buf.set(chunk);
+      i = chunk.byteLength;
+
+      return parsed[0];
     }
 
-    const chunk = data.subarray(parsed[1]);
-    buf.set(chunk);
-    i = chunk.byteLength;
-
-    return parsed[0];
+    const n = await Deno.stdin.read(buf.subarray(i));
+    if (typeof n === "number") {
+      i += n;
+    }
   }
 }
 
 const dec = new TextDecoder();
 
 export function readRegExp(re: RegExp): RegExpMatchArray {
-  for (let i = 0; i < 10; i += 1) {
-    const data = readSync();
-
+  for (let i = 0; i < 100; i += 1) {
+    const data = buf.subarray(0, i);
     const match = dec.decode(data).match(re);
-    if (!match) {
-      continue;
+
+    if (match) {
+      const chunk = data.subarray(match.index! + match[0].length);
+      buf.set(chunk);
+      i = chunk.byteLength;
+
+      return match;
     }
 
-    const chunk = data.subarray(match.index! + match[0].length);
-    buf.set(chunk);
-    i = chunk.byteLength;
-
-    return match;
+    const n = Deno.stdin.readSync(buf.subarray(i));
+    if (typeof n === "number") {
+      i += n;
+    }
   }
 
   throw new Error(`readRegExp error: [${buf.subarray(0, i)}]`);
-}
-
-async function readAsync(): Promise<Uint8Array> {
-  const n = await Deno.stdin.read(buf.subarray(i));
-  if (typeof n === "number") {
-    i += n;
-  }
-  return buf.subarray(0, i);
-}
-
-function readSync(): Uint8Array {
-  const n = Deno.stdin.readSync(buf.subarray(i));
-  if (typeof n === "number") {
-    i += n;
-  }
-  return buf.subarray(0, i);
 }
