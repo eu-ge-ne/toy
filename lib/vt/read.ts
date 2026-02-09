86c9814fc6d5270303990acf2b0e70256f45be80
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-08 21:33:40 -0500

    1

--- lib/vt/read.ts
+++ lib/vt/read.ts
@@ -2,24 +2,16 @@ import { Key } from "@lib/kitty";
 
 const buf = new Uint8Array(1024);
 let i = 0;
-const waiters: PromiseWithResolvers<void>[] = [];
-
-export async function listen(): Promise<void> {
-  for await (const chunk of Deno.stdin.readable) {
-    buf.set(chunk, i);
-    i += chunk.byteLength;
-
-    if (waiters.length !== 0) {
-      for (const waiter of waiters) {
-        waiter.resolve();
-      }
-      waiters.length = 0;
-    }
-  }
-}
 
 export async function readKey(): Promise<Key> {
   while (true) {
+    const n = await Deno.stdin.read(buf.subarray(i));
+    if (typeof n === "number") {
+      i += n;
+    } else {
+      continue;
+    }
+
     const data = buf.subarray(0, i);
 
     const parsed = Key.parse(data);
@@ -31,38 +23,32 @@ export async function readKey(): Promise<Key> {
 
       return parsed[0];
     }
-
-    const waiter = Promise.withResolvers<void>();
-    waiters.push(waiter);
-    await waiter.promise;
   }
 }
 
-export async function* __read(): AsyncGenerator<Key | Uint8Array> {
-  const bytes_read = await Deno.stdin.read(buf);
-  if (bytes_read === null) {
-    return;
-  }
-  const bytes = buf.subarray(0, bytes_read);
+const dec = new TextDecoder();
 
-  for (let i = 0; i < bytes.length;) {
-    const result = Key.parse(bytes.subarray(i));
-
-    if (!result) {
-      let next_esc_i = bytes.indexOf(0x1b, i + 1);
-      if (next_esc_i < 0) {
-        next_esc_i = bytes.length;
-      }
-
-      yield bytes.subarray(i, next_esc_i);
-
-      i = next_esc_i;
-
+export function readRegExp(re: RegExp): RegExpMatchArray {
+  for (let attemt = 0; attemt < 10; attemt += 1) {
+    const n = Deno.stdin.readSync(buf.subarray(i));
+    if (typeof n === "number") {
+      i += n;
+    } else {
       continue;
     }
 
-    yield result[0];
+    const data = buf.subarray(0, i);
 
-    i += result[1];
+    const match = dec.decode(data).match(re);
+    if (match) {
+      const chunk = data.subarray(match.index! + match[0].length);
+
+      buf.set(chunk);
+      i = chunk.byteLength;
+
+      return match;
+    }
   }
+
+  throw new Error(`readRegExp error: [${buf.subarray(0, i)}]`);
 }
