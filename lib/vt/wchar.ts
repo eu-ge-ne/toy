ac1faea168e972808d730577e0f3fa3f9304d951
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-09 19:14:33 -0500

    1

--- lib/vt/wchar.ts
+++ lib/vt/wchar.ts
@@ -3,7 +3,7 @@ import { set } from "./cursor.ts";
 import { readSync } from "./read.ts";
 import { sync } from "./writer.ts";
 
-export const cprReq = CSI("6n");
+export const cprReq = CSI("6n"); // Reply: CSI 1 ; 6 R
 
 const dec = new TextDecoder();
 
@@ -13,13 +13,49 @@ export function wchar(y: number, x: number, bytes: Uint8Array): number {
   sync.write(cprReq);
 
   const x1 = readSync((data) => {
-    const match = dec.decode(data).match(/\x1b\[\d+;(\d+)R/);
-    if (!match) {
+    // CSI
+    let iStart = -1;
+    for (let i = 0; i < data.byteLength; i += 1) {
+      if (data[i] === 0x1b) {
+        iStart = i;
+        break;
+      }
+    }
+    if (iStart === -1) {
       return undefined;
     }
-    const x = Number.parseInt(match[1]!);
-    const n = match.index! + match[0].length;
-    return [x, n];
+    iStart += 1;
+    if (data[iStart] !== 0x5b) {
+      return undefined;
+    }
+
+    // ;
+    let iSemicolon = -1;
+    for (let i = iStart + 1; i < data.byteLength; i += 1) {
+      if (data[i] === 0x3b) {
+        iSemicolon = i;
+        break;
+      }
+    }
+    if (iSemicolon === -1) {
+      return undefined;
+    }
+
+    // R
+    let iR = -1;
+    for (let i = iSemicolon + 1; i < data.byteLength; i += 1) {
+      if (data[i] === 0x52) {
+        iR = i;
+        break;
+      }
+    }
+    if (iR === -1) {
+      return undefined;
+    }
+
+    const x = Number.parseInt(dec.decode(data.subarray(iSemicolon + 1, iR)));
+
+    return [x, iR + 1];
   });
 
   return x1 - 1 - x;
