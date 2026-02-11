7f5097150437cf895e8821d9556521761ec1815a
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-11 00:42:01 -0500

    1

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+dir=$(dirname "$0")
+
+rm -rf "$dir/../tmp/test"
 deno check --all --frozen --reload
-deno test --doc --coverage=tmp/cov/ --coverage-raw-data-only --junit-path=tmp/junit.xml
-deno coverage --lcov tmp/cov/ > tmp/lcov.info
+deno test \
+    --doc \
+    --coverage="$dir/../tmp/test/cov" \
+    --coverage-raw-data-only \
+    --junit-path="$dir/../tmp/test/junit.xml"
+deno coverage --lcov "$dir/../tmp/test/cov" > "$dir/../tmp/test/lcov"
