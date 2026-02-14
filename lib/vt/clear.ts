ac2467629b3e3f10a3374cb85592cf3b0e9d84e2
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-14 10:41:40 -0500

    feat: @components/area

--- lib/vt/clear.ts
+++ lib/vt/clear.ts
@@ -1,19 +1,5 @@
-import * as cursor from "./cursor.ts";
 import { ech } from "./ech.ts";
 import { Writer } from "./writer.ts";
-
-export function clear_area(
-  out: Writer,
-  { y, x, w, h }: { y: number; x: number; w: number; h: number },
-): void {
-  cursor.set(out, y, x);
-
-  for (let i = h; i > 0; i -= 1) {
-    ech(out, w);
-
-    out.write(cursor.down);
-  }
-}
 
 export function clear_line(out: Writer, w: number): void {
   ech(out, w);
