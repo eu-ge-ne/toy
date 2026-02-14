e82b6930596c57ac63db790c19009eafab5fada6
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-02-13 20:47:20 -0500

    1

--- lib/ui/component.ts
+++ lib/ui/component.ts
@@ -7,8 +7,12 @@ export abstract class Component implements Area {
   y = 0;
   x = 0;
 
-  constructor(readonly layout: (_: Area, __: Area) => void) {
+  constructor(readonly resize: (_: Area, __: Area) => void) {
   }
 
   abstract render(): void;
+
+  layout(p: Area): void {
+    this.resize(this, p);
+  }
 }
