7887b5f282f0faa90704d5b08db63868ef43fd1e
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-04-08 21:58:56 -0400

    1

--- src/lib/ui/modal.ts
+++
@@ -1,5 +0,0 @@
-import { Widget } from "./widget.ts";
-
-export abstract class Modal<T = unknown> extends Widget<T> {
-  abstract open(): void;
-}
--- src/lib/ui/ui.ts
+++ src/lib/ui/ui.ts
@@ -1,5 +1,4 @@
 export * from "./bg.ts";
 export * from "./frame.ts";
 export * from "./list.ts";
-export * from "./modal.ts";
 export * from "./text.ts";
