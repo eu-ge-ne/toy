36933da2f240b2a5f81f1373f37908ccbff10604
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-05-10 22:27:28 -0400

    1

--- src/libs/api/doc.ts
+++ src/libs/api/doc.ts
@@ -1,4 +1,15 @@
+import * as events from "@libs/events";
+
+export type DocInterceptorEvents = Record<PropertyKey, never>;
+
+export type DocReactorEvents = {
+  "change": (_: { modified: boolean; lineCount: number }) => void;
+  "change.name": (_: string) => void;
+};
+
 export type DocApi = {
+  events: events.Listener<DocInterceptorEvents, DocReactorEvents>;
+
   open(_: string): Promise<void>;
   save(): Promise<void>;
   saveAs(): Promise<void>;
