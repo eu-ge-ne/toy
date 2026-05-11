a67f9350da313e19e46be42b672a4606b82848f5
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-05-10 21:59:36 -0400

    1

--- src/libs/api/cursor.ts
+++ src/libs/api/cursor.ts
@@ -1,6 +1,6 @@
 import * as events from "@libs/events";
 
-export type CursorInterceptorEvents = {};
+export type CursorInterceptorEvents = Record<PropertyKey, never>;
 
 export type CursorReactorEvents = {
   "changed": (_: { ln: number; col: number }) => void;
