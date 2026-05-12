aace104d0de1075d31bbde2dffee51bca69810fa
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-05-11 23:01:38 -0400

    1

--- src/libs/plugins/plugin.ts
+++ src/libs/plugins/plugin.ts
@@ -1,31 +1,17 @@
-import {
-  AboutAPI,
-  AlertModalAPI,
-  API,
-  ConfirmModalAPI,
-  CursorAPI,
-  DebugAPI,
-  DocAPI,
-  FileNameModalAPI,
-  IOAPI,
-  PaletteModalAPI,
-  RuntimeAPI,
-  ThemeAPI,
-  ZenAPI,
-} from "@libs/api";
+import * as api from "@libs/api";
 
 export type Plugin = {
-  init?(_: API): void;
-  initRuntime?(_: API): RuntimeAPI;
-  initIO?(_: API): IOAPI;
-  initDebug?(_: API): DebugAPI;
-  initCursor?(_: API): CursorAPI;
-  initDoc?(_: API): DocAPI;
-  initTheme?(_: API): ThemeAPI;
-  initZen?(_: API): ZenAPI;
-  initAbout?(_: API): AboutAPI;
-  initAlertModal?(_: API): AlertModalAPI;
-  initConfirmModal?(_: API): ConfirmModalAPI;
-  initFileNameModal?(_: API): FileNameModalAPI;
-  initPaletteModal?(_: API): PaletteModalAPI;
+  init?(_: api.API): void;
+  initRuntime?(_: api.API): api.RuntimeAPI;
+  initIO?(_: api.API): api.IOAPI;
+  initDebug?(_: api.API): api.DebugAPI;
+  initCursor?(_: api.API): api.CursorAPI;
+  initDoc?(_: api.API): api.DocAPI;
+  initTheme?(_: api.API): api.ThemeAPI;
+  initZen?(_: api.API): api.ZenAPI;
+  initAbout?(_: api.API): api.AboutAPI;
+  initAlertModal?(_: api.API): api.AlertModalAPI;
+  initConfirmModal?(_: api.API): api.ConfirmModalAPI;
+  initFileNameModal?(_: api.API): api.FileNameModalAPI;
+  initPaletteModal?(_: api.API): api.PaletteModalAPI;
 };
