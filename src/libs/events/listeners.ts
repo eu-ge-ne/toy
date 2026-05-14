d4227ec875f456c16aabafa808c654ce91491a76
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-05-14 00:26:13 -0400

    2

--- src/libs/events/listeners.ts
+++ src/libs/events/listeners.ts
@@ -2,55 +2,28 @@ import { EventClients, SignalClients } from "./clients.ts";
 import { Events } from "./events.ts";
 import { Signals } from "./signals.ts";
 
-export class Listener<T1 extends Events, T2 extends Signals> {
-  constructor(
-    private readonly eventClients: EventClients<T1>,
-    private readonly signalClients: SignalClients<T2>,
-  ) {
+export class EventListener<T extends Events> {
+  constructor(private readonly clients: EventClients<T>) {
   }
 
-  intercept<K extends keyof T1>(name: K, fn: T1[K]): () => void {
-    return this.interceptOrdered(name, 0, fn);
+  on<K extends keyof T>(name: K, fn: T[K]): () => void {
+    return this.onOrdered(name, 0, fn);
   }
 
-  react<K extends keyof T2>(name: K, fn: T2[K]): () => void {
-    return this.reactOrdered(name, 0, fn);
-  }
-
-  interceptOrdered<K extends keyof T1>(
-    name: K,
-    order: number,
-    fn: T1[K],
-  ): () => void {
-    let xx = this.eventClients[name];
+  onOrdered<K extends keyof T>(name: K, order: number, fn: T[K]): () => void {
+    let xx = this.clients[name];
     if (!xx) {
-      xx = this.eventClients[name] = [];
+      xx = this.clients[name] = [];
     }
 
     xx.push({ fn, order });
     xx.sort((a, b) => a.order - b.order);
 
-    return () => this.#offIntercept(name, fn);
+    return () => this.#off(name, fn);
   }
 
-  reactOrdered<K extends keyof T2>(
-    name: K,
-    order: number,
-    fn: T2[K],
-  ): () => void {
-    let xx = this.signalClients[name];
-    if (!xx) {
-      xx = this.signalClients[name] = [];
-    }
-
-    xx.push({ fn, order });
-    xx.sort((a, b) => a.order - b.order);
-
-    return () => this.#offReact(name, fn);
-  }
-
-  #offIntercept<K extends keyof T1>(name: K, fn: T1[K]): void {
-    const xx = this.eventClients[name];
+  #off<K extends keyof T>(name: K, fn: T[K]): void {
+    const xx = this.clients[name];
     if (!xx) {
       return;
     }
@@ -60,9 +33,30 @@ export class Listener<T1 extends Events, T2 extends Signals> {
       xx.splice(i, 1);
     }
   }
+}
 
-  #offReact<K extends keyof T2>(name: K, fn: T2[K]): void {
-    const xx = this.signalClients[name];
+export class SignalListener<T extends Signals> {
+  constructor(private readonly clients: SignalClients<T>) {
+  }
+
+  on<K extends keyof T>(name: K, fn: T[K]): () => void {
+    return this.onOrdered(name, 0, fn);
+  }
+
+  onOrdered<K extends keyof T>(name: K, order: number, fn: T[K]): () => void {
+    let xx = this.clients[name];
+    if (!xx) {
+      xx = this.clients[name] = [];
+    }
+
+    xx.push({ fn, order });
+    xx.sort((a, b) => a.order - b.order);
+
+    return () => this.#off(name, fn);
+  }
+
+  #off<K extends keyof T>(name: K, fn: T[K]): void {
+    const xx = this.clients[name];
     if (!xx) {
       return;
     }
