70a511b17fece442cb1d2b248a14db6a23df10fe
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-05-16 22:51:41 -0400

    fix: @libs/toy

--- src/libs/toy/toy.ts
+++ src/libs/toy/toy.ts
@@ -1,21 +1,21 @@
 import * as api from "@libs/api";
 import * as plugins from "@libs/plugins";
 
-const paths = [
-  "@plugins/alert-modal",
-  "@plugins/confirm-modal",
-  "@plugins/debug",
-  "@plugins/doc",
-  "@plugins/file-name-modal",
-  "@plugins/footer",
-  "@plugins/header",
-  "@plugins/io",
-  "@plugins/palette-modal",
-  "@plugins/runtime",
-  "@plugins/shortcuts",
-  "@plugins/theme",
-  "@plugins/zen",
-];
+import alertModal from "@plugins/alert-modal";
+import confirmModal from "@plugins/confirm-modal";
+import debug from "@plugins/debug";
+import doc from "@plugins/doc";
+import fileNameModal from "@plugins/file-name-modal";
+import footer from "@plugins/footer";
+import header from "@plugins/header";
+import io from "@plugins/io";
+import paletteModal from "@plugins/palette-modal";
+import runtime from "@plugins/runtime";
+import shortcuts from "@plugins/shortcuts";
+import theme from "@plugins/theme";
+import zen from "@plugins/zen";
+
+const userPlugins: string[] = [];
 
 export class Toy extends api.Toy {
   #plugins: plugins.Plugin[] = [];
@@ -26,12 +26,28 @@ export class Toy extends api.Toy {
 
   static async load(): Promise<Toy> {
     const toy = new Toy();
-    await toy.#load();
+
+    toy.#register(alertModal);
+    toy.#register(confirmModal);
+    toy.#register(debug);
+    toy.#register(doc);
+    toy.#register(fileNameModal);
+    toy.#register(footer);
+    toy.#register(header);
+    toy.#register(io);
+    toy.#register(paletteModal);
+    toy.#register(runtime);
+    toy.#register(shortcuts);
+    toy.#register(theme);
+    toy.#register(zen);
+
+    await toy.#loadUserPlugins();
+
     return toy;
   }
 
-  async #load(): Promise<void> {
-    await Promise.all(paths.map(async (x) => this.#register((await import(x)).default)));
+  async #loadUserPlugins(): Promise<void> {
+    await Promise.all(userPlugins.map(async (x) => this.#register((await import(x)).default)));
 
     for (const plugin of this.#plugins) {
       plugin.init?.(this);
