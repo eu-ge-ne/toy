c3cff8f9df1945be3262b112dd7936252fbadc60
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-06-07 23:57:22 -0400

    refactor: @libs/api

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 import { parseArgs } from "@std/cli/parse-args";
 
+import * as alertModal from "@libs/alert-modal";
+import * as api from "@libs/api";
+import * as buffers from "@libs/buffers";
+import * as confirmModal from "@libs/confirm-modal";
+import * as debug from "@libs/debug";
+import * as fileNameModal from "@libs/file-name-modal";
+import * as footer from "@libs/footer";
+import * as header from "@libs/header";
+import * as io from "@libs/io";
+import * as paletteModal from "@libs/palette-modal";
+import * as plugins from "@libs/plugins";
+import * as runtime from "@libs/runtime";
+import * as shortcuts from "@libs/shortcuts";
 import * as std from "@libs/std";
-import { Toy } from "@libs/toy";
+import * as themes from "@libs/themes";
+import * as views from "@libs/views";
+import * as zen from "@libs/zen";
 
 export const args = parseArgs(Deno.args, {
   boolean: ["version"],
@@ -13,6 +28,53 @@ export const args = parseArgs(Deno.args, {
 if (args.version) {
   console.log(std.version);
   Deno.exit();
+}
+
+class Toy extends api.Toy {
+  #plugins: plugins.Plugin[] = [];
+
+  private constructor() {
+    super();
+  }
+
+  static async load(): Promise<Toy> {
+    const toy = new Toy();
+
+    toy.#register(alertModal.plugin);
+    toy.#register(buffers.plugin);
+    toy.#register(confirmModal.plugin);
+    toy.#register(debug.plugin);
+    toy.#register(fileNameModal.plugin);
+    toy.#register(footer.plugin);
+    toy.#register(header.plugin);
+    toy.#register(io.plugin);
+    toy.#register(paletteModal.plugin);
+    toy.#register(runtime.plugin);
+    toy.#register(shortcuts.plugin);
+    toy.#register(themes.plugin);
+    toy.#register(views.plugin);
+    toy.#register(zen.plugin);
+
+    for (const plugin of toy.#plugins) {
+      plugin.init?.(toy);
+    }
+
+    return toy;
+  }
+
+  #register(plugin: plugins.Plugin): void {
+    this.#plugins.push(plugin);
+
+    // deno-lint-ignore no-explicit-any
+    const rawThis = this as any;
+
+    for (const [apiName, apiFn] of Object.entries(plugin.register ?? {})) {
+      if (rawThis[apiName]) {
+        throw new Error("API conflict");
+      }
+      rawThis[apiName] = apiFn(this);
+    }
+  }
 }
 
 const toy = await Toy.load();
