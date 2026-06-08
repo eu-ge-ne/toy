aa9072621b4c62075ef95af747cefcaa5259fd8e
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-06-07 23:12:08 -0400

    refactor: @libs/debug

--- src/libs/api/index.ts
+++ src/libs/api/index.ts
@@ -1,6 +1,5 @@
 export * from "./alert-modal.ts";
 export * from "./confirm-modal.ts";
-export * from "./debug.ts";
 export * from "./file-name-modal.ts";
 export * from "./palette-modal.ts";
 export * from "./toy.ts";
--- src/libs/debug/plugin.ts
+++ src/libs/debug/plugin.ts
@@ -3,46 +3,16 @@ import * as plugins from "@libs/plugins";
 import * as std from "@libs/std";
 import * as themes from "@libs/themes";
 
+import { DebugAPI } from "./api.ts";
 import { DebugWidget } from "./widget.ts";
-
-let widget: DebugWidget;
-
-function resize(toy: api.Toy): void {
-  const { columns, rows } = Deno.consoleSize();
-
-  const w = std.clamp(30, 0, columns);
-  const h = std.clamp(10, 0, rows);
-  const y = toy.zen.enabled ? rows - h : rows - 1 - h;
-  const x = columns - w;
-
-  widget.resize(w, h, y, x);
-}
 
 const MIB = Math.pow(1024, 2);
 
-function memUsage(): { rss: number; heapTotal: number; heapUsed: number; external: number } {
-  const mem = Deno.memoryUsage();
+let widget: DebugWidget;
 
-  return {
-    rss: mem.rss / MIB,
-    heapTotal: mem.heapTotal / MIB,
-    heapUsed: mem.heapUsed / MIB,
-    external: mem.external / MIB,
-  };
-}
-
-export default {
-  init(toy: api.Toy): void {
-    widget = new DebugWidget();
-
-    widget.version = std.version;
-
-    toy.theme.signals.on("change")((x) => widget.setTheme(themes.Themes[x]));
-    toy.io.signals.on("render", 1000)(() => widget.render());
-    toy.io.signals.on("resize")(() => resize(toy));
-  },
+export const plugin = {
   register: {
-    debug(_: api.Toy): api.Debug {
+    debug(_: api.Toy): DebugAPI {
       let timer: NodeJS.Timeout;
 
       function updateMemUsage(): void {
@@ -76,4 +46,36 @@ export default {
       };
     },
   },
+
+  init(toy: api.Toy): void {
+    widget = new DebugWidget();
+
+    widget.version = std.version;
+
+    toy.theme.signals.on("change")((x) => widget.setTheme(themes.Themes[x]));
+    toy.io.signals.on("render", 1000)(() => widget.render());
+    toy.io.signals.on("resize")(() => resize(toy));
+  },
 } satisfies plugins.Plugin;
+
+function resize(toy: api.Toy): void {
+  const { columns, rows } = Deno.consoleSize();
+
+  const w = std.clamp(30, 0, columns);
+  const h = std.clamp(10, 0, rows);
+  const y = toy.zen.enabled ? rows - h : rows - 1 - h;
+  const x = columns - w;
+
+  widget.resize(w, h, y, x);
+}
+
+function memUsage(): { rss: number; heapTotal: number; heapUsed: number; external: number } {
+  const mem = Deno.memoryUsage();
+
+  return {
+    rss: mem.rss / MIB,
+    heapTotal: mem.heapTotal / MIB,
+    heapUsed: mem.heapUsed / MIB,
+    external: mem.external / MIB,
+  };
+}
