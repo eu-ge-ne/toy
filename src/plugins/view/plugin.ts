4d978ce49d4c943c5490ca07d61b8a3d6b5a71f1
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-06-13 21:01:59 -0400

    1

--- src/plugins/view/plugin.ts
+++ src/plugins/view/plugin.ts
@@ -7,71 +7,64 @@ import { IOAPI } from "@plugins/io";
 import { ThemesAPI } from "@plugins/themes";
 import { ZenAPI } from "@plugins/zen";
 
-export type ViewAPI = {
-  view: {
-    signals: libEvents.Listener<ViewSignals>;
-    toggleWhitespace(): void;
-    toggleWrap(): void;
-    selectAll(): void;
-    copy(): void;
-    cut(): void;
-    paste(): void;
-  };
-};
+export type ViewAPI = ReturnType<typeof ViewPlugin>;
 
-type ViewSignals = {
-  "change.cursor": (_: { ln: number; col: number }) => void;
-};
-
-export function ViewPlugin(api: ThemesAPI & BufferAPI & ZenAPI & IOAPI): ViewAPI {
-  const signals = new libEvents.SignalEmitter<ViewSignals>();
-
-  const widget = new widgets.Editor(api.buffer, {
-    multiLine: true,
-    onCursorChange: (x) => signals.broadcast("change.cursor", { ln: x.ln, col: x.col }),
-  });
-
-  api.theme.signals.on("change")((x) => widget.setTheme(libThemes.Themes[x]));
-  api.zen.signals.on("toggle")(() => widget.toggleIndex());
-
-  api.io.events.on("key.press")(async ({ key }) => widget.onKeyPress(key));
-  api.io.signals.on("render")(() => widget.render());
-  api.io.signals.on("resize")(() => {
-    const { columns, rows } = Deno.consoleSize();
-    if (api.zen.enabled) {
-      widget.resize(columns, rows, 0, 0);
-    } else {
-      widget.resize(columns, rows - 2, 1, 0);
-    }
-  });
-
+export function ViewPlugin(...api: ConstructorParameters<typeof View>) {
   return {
-    view: {
-      signals: signals.listener,
-
-      toggleWhitespace(): void {
-        widget.toggleWhitespace();
-      },
-
-      toggleWrap(): void {
-        widget.toggleWrap();
-      },
-
-      selectAll(): void {
-        widget.selectAll();
-      },
-
-      copy(): void {
-        widget.copy();
-      },
-
-      cut(): void {
-        widget.cut();
-      },
-
-      paste(): void {
-        widget.paste();
-      },
-    },
+    view: new View(...api),
   };
+}
+
+class View {
+  private readonly widget: widgets.Editor;
+  private readonly emitter = new libEvents.SignalEmitter<{
+    "change.cursor": (_: { ln: number; col: number }) => void;
+  }>();
+
+  constructor(private readonly api: ThemesAPI & BufferAPI & ZenAPI & IOAPI) {
+    this.widget = new widgets.Editor(api.buffer, {
+      multiLine: true,
+      onCursorChange: (x) => this.emitter.broadcast("change.cursor", { ln: x.ln, col: x.col }),
+    });
+
+    api.theme.signals.on("change")((x) => this.widget.setTheme(libThemes.Themes[x]));
+    api.zen.signals.on("toggle")(() => this.widget.toggleIndex());
+
+    api.io.events.on("key.press")(async ({ key }) => this.widget.onKeyPress(key));
+    api.io.signals.on("render")(() => this.widget.render());
+    api.io.signals.on("resize")(() => {
+      const { columns, rows } = Deno.consoleSize();
+      if (api.zen.enabled) {
+        this.widget.resize(columns, rows, 0, 0);
+      } else {
+        this.widget.resize(columns, rows - 2, 1, 0);
+      }
+    });
+  }
+
+  readonly signals = this.emitter.listener;
+
+  toggleWhitespace(): void {
+    this.widget.toggleWhitespace();
+  }
+
+  toggleWrap(): void {
+    this.widget.toggleWrap();
+  }
+
+  selectAll(): void {
+    this.widget.selectAll();
+  }
+
+  copy(): void {
+    this.widget.copy();
+  }
+
+  cut(): void {
+    this.widget.cut();
+  }
+
+  paste(): void {
+    this.widget.paste();
+  }
 }
