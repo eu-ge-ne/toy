869cdb0dfe61983d3764178e5cf3ccc4589ce55f
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-06-13 20:46:30 -0400

    1

--- src/plugins/io/plugin.ts
+++ src/plugins/io/plugin.ts
@@ -4,82 +4,76 @@ import * as vt from "@libs/vt";
 
 import { RuntimeAPI } from "@plugins/runtime";
 
-export type IOAPI = {
-  io: {
-    events: libEvents.Listener<IOEvents>;
-    signals: libEvents.Listener<IOSignals>;
-    resize(): void;
-    loop(_: () => unknown): Promise<void>;
+export type IOAPI = ReturnType<typeof IOPlugin>;
+
+export function IOPlugin(...api: ConstructorParameters<typeof IO>) {
+  return {
+    io: new IO(...api),
   };
-};
+}
 
-type IOEvents = {
-  "key.press": (_: libEvents.EventData<{ key: kitty.Key }>) => Promise<void>;
-};
+class IO {
+  private readonly eventEmitter = new libEvents.EventEmitter<{
+    "key.press": (_: libEvents.EventData<{ key: kitty.Key }>) => Promise<void>;
+  }>();
 
-type IOSignals = {
-  "resize": () => void;
-  "render": () => void;
-  "render.completed": (_: number) => void;
-  "key.handled": (_: number) => void;
-};
+  private readonly signalEmitter = new libEvents.SignalEmitter<{
+    "resize": () => void;
+    "render": () => void;
+    "render.completed": (_: number) => void;
+    "key.handled": (_: number) => void;
+  }>();
 
-export function IOPlugin(api: RuntimeAPI): IOAPI {
-  const events = new libEvents.EventEmitter<IOEvents>();
-  const signals = new libEvents.SignalEmitter<IOSignals>();
+  constructor(private readonly api: RuntimeAPI) {
+    api.runtime.events.on("start")(async () => {
+      vt.init();
 
-  function resize(): void {
-    signals.broadcast("resize");
+      Deno.addSignalListener("SIGWINCH", () => {
+        this.resize();
+        this.#render();
+      });
+    });
+
+    api.runtime.events.on("stop", 1000)(async () => vt.restore());
   }
 
-  function render(): void {
+  events = this.eventEmitter.listener;
+  signals = this.signalEmitter.listener;
+
+  resize(): void {
+    this.signalEmitter.broadcast("resize");
+  }
+
+  async loop(stop: () => unknown): Promise<void> {
+    while (!stop()) {
+      this.#render();
+
+      const key = await vt.readKey();
+
+      await this.#keyPress(key);
+    }
+  }
+
+  #render(): void {
     const t0 = performance.now();
 
     vt.sync.bsu();
     vt.buf.write(vt.cursor.hide);
 
-    signals.broadcast("render");
+    this.signalEmitter.broadcast("render");
 
     vt.buf.write(vt.cursor.show);
     vt.buf.flush();
     vt.sync.esu();
 
-    signals.broadcast("render.completed", performance.now() - t0);
+    this.signalEmitter.broadcast("render.completed", performance.now() - t0);
   }
 
-  async function keyPress(key: kitty.Key): Promise<void> {
+  async #keyPress(key: kitty.Key): Promise<void> {
     const t0 = performance.now();
 
-    await events.dispatch("key.press", { key });
+    await this.eventEmitter.dispatch("key.press", { key });
 
-    signals.broadcast("key.handled", performance.now() - t0);
+    this.signalEmitter.broadcast("key.handled", performance.now() - t0);
   }
-
-  api.runtime.events.on("start")(async () => {
-    vt.init();
-
-    Deno.addSignalListener("SIGWINCH", () => {
-      resize();
-      render();
-    });
-  });
-
-  api.runtime.events.on("stop", 1000)(async () => vt.restore());
-
-  return {
-    io: {
-      events: events.listener,
-      signals: signals.listener,
-      resize,
-      async loop(stop: () => unknown): Promise<void> {
-        while (!stop()) {
-          render();
-
-          const key = await vt.readKey();
-
-          await keyPress(key);
-        }
-      },
-    },
-  };
 }
