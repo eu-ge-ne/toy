9e408c9c6b0302719033ddaad2e030a7a01c476e
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-06-13 20:17:40 -0400

    1

--- src/plugins/confirm-modal/plugin.ts
+++ src/plugins/confirm-modal/plugin.ts
@@ -6,66 +6,66 @@ import { ThemesAPI } from "@plugins/themes";
 
 import { AskWidget } from "./widget.ts";
 
-export type ConfirmModalAPI = {
-  confirmModal: {
-    open(_: string): Promise<boolean>;
-  };
-};
+export type ConfirmModalAPI = ReturnType<typeof ConfirmModalPlugin>;
 
-export function ConfirmModalPlugin(api: ThemesAPI & IOAPI): ConfirmModalAPI {
-  const widget = new AskWidget();
-
-  api.theme.signals.on("change")((x) => widget.setTheme(libThemes.Themes[x]));
-
-  api.io.signals.on("resize")(() => {
-    const { columns, rows } = Deno.consoleSize();
-
-    const w = std.clamp(60, 0, columns);
-    const h = std.clamp(7, 0, rows);
-    const y = Math.trunc((rows - h) / 2);
-    const x = Math.trunc((columns - w) / 2);
-
-    widget.resize(w, h, y, x);
-  });
-
+export function ConfirmModalPlugin(api: ConstructorParameters<typeof ConfirmModal>[0]) {
   return {
-    confirmModal: {
-      async open(message: string): Promise<boolean> {
-        let opened = true;
-        let result = false;
-
-        widget.children.text.value = message;
-
-        const offRender = api.io.signals.on("render", 1000)(() => widget.render());
-
-        const offKeyPress = api.io.events.on("key.press", -1000)(
-          async (data) => {
-            data.cancel = true;
-
-            switch (data.key.name) {
-              case "ESC":
-                result = false;
-                opened = false;
-                break;
-              case "ENTER":
-                result = true;
-                opened = false;
-                break;
-            }
-
-            if (opened) {
-              return;
-            }
-
-            offRender();
-            offKeyPress();
-          },
-        );
-
-        await api.io.loop(() => !opened);
-
-        return result;
-      },
-    },
+    confirmModal: new ConfirmModal(api),
   };
+}
+
+class ConfirmModal {
+  private readonly widget = new AskWidget();
+
+  constructor(private readonly api: ThemesAPI & IOAPI) {
+    api.theme.signals.on("change")((x) => this.widget.setTheme(libThemes.Themes[x]));
+
+    api.io.signals.on("resize")(() => {
+      const { columns, rows } = Deno.consoleSize();
+
+      const w = std.clamp(60, 0, columns);
+      const h = std.clamp(7, 0, rows);
+      const y = Math.trunc((rows - h) / 2);
+      const x = Math.trunc((columns - w) / 2);
+
+      this.widget.resize(w, h, y, x);
+    });
+  }
+
+  async open(message: string): Promise<boolean> {
+    let opened = true;
+    let result = false;
+
+    this.widget.children.text.value = message;
+
+    const offRender = this.api.io.signals.on("render", 1000)(() => this.widget.render());
+
+    const offKeyPress = this.api.io.events.on("key.press", -1000)(
+      async (data) => {
+        data.cancel = true;
+
+        switch (data.key.name) {
+          case "ESC":
+            result = false;
+            opened = false;
+            break;
+          case "ENTER":
+            result = true;
+            opened = false;
+            break;
+        }
+
+        if (opened) {
+          return;
+        }
+
+        offRender();
+        offKeyPress();
+      },
+    );
+
+    await this.api.io.loop(() => !opened);
+
+    return result;
+  }
 }
