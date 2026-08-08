fa465f6f25ed4492fb58d480b21415d282fdd194
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-08-08 13:32:26 -0400

    refactor: @libs/documents/slice

--- src/libs/documents/slice.ts
+++ src/libs/documents/slice.ts
@@ -1,7 +1,59 @@
+import { Buf } from "./buf.ts";
+
 export class Slice {
-  constructor(readonly start: number, readonly end: number) {}
+  #start = 0;
+  #end = 0;
+  #eolStart = 0;
+  #eolEnd = 0;
+
+  constructor(start: number, end: number, eolStart: number, eolEnd: number) {
+    this.#start = start;
+    this.#end = end;
+    this.#eolStart = eolStart;
+    this.#eolEnd = eolEnd;
+  }
+
+  static create(buf: Buf, start: number, end: number): Slice {
+    return new Slice(start, end, buf.charToLine(start), buf.charToLine(end));
+  }
+
+  clone(): Slice {
+    return new Slice(this.#start, this.#end, this.#eolStart, this.#eolEnd);
+  }
 
   get length(): number {
-    return this.end - this.start;
+    return this.#end - this.#start;
+  }
+
+  get eolLength(): number {
+    return this.#eolEnd - this.#eolStart;
+  }
+
+  get start(): number {
+    return this.#start;
+  }
+
+  get end(): number {
+    return this.#end;
+  }
+
+  get eolStart(): number {
+    return this.#eolStart;
+  }
+
+  get eolEnd(): number {
+    return this.#eolEnd;
+  }
+
+  setStart(buf: Buf, start: number): void {
+    this.#start = start;
+
+    this.#eolStart = buf.charToLine(start);
+  }
+
+  setEnd(buf: Buf, end: number): void {
+    this.#end = end;
+
+    this.#eolEnd = buf.charToLine(end);
   }
 }
