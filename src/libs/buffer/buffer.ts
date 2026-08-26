e3e94d118d6528a1bf08db1cab98576480c6cc0e
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-08-25 20:38:31 -0400

    1

--- src/libs/buffer/buffer.ts
+++ src/libs/buffer/buffer.ts
@@ -112,6 +112,29 @@ export class Buffer {
     );
   }
 
+  scanLine(
+    ln: number,
+    cb: (
+      gr: Grapheme,
+      i: number,
+    ) => true | undefined,
+  ): void {
+    let gr: Grapheme;
+    let i = 0;
+
+    for (const chunk of this.#str.read2(ln, 0, ln + 1, 0)) {
+      for (const { segment } of sgr.segment(chunk)) {
+        gr = GRAPHEMES.get(segment);
+
+        if (cb(gr, i)) {
+          break;
+        }
+
+        i += 1;
+      }
+    }
+  }
+
   scanLineWrap(
     ln: number,
     cb: (
@@ -349,7 +372,7 @@ export class Buffer {
   #unitPos(ln: number, col: number): [number, number] {
     let unitCol = 0;
 
-    this.scanLineWrap(ln, (gr, i) => {
+    this.scanLine(ln, (gr, i) => {
       if (i === col) {
         return true;
       }
